320819898950cb139a0b762c20901ee08fb9e2d5
Yuri Furusho <yuri.furusho.positivo@gmail.com>
2018-06-27 09:38:05 -0300

    Force Crash Button Added

--- app/src/main/java/com/yuricfurusho/temp01firebase/MainActivity.kt
+++ app/src/main/java/com/yuricfurusho/temp01firebase/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.yuricfurusho.temp01firebase
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import com.crashlytics.android.Crashlytics
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        fab.setOnClickListener { view -> Crashlytics.getInstance().crash() }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
