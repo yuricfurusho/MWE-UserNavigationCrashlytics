4fc05b969040769ef165972f48fbf9821cad04cd
Yuri Furusho <yuri.furusho.positivo@gmail.com>
2018-07-13 15:53:43 -0300

    disparo de custom event quando cllica no botao

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+y
+Iapp/src/main/java/com/yuricfurusho/temp01firebase/MainActivityFragment.kt,9\5\95bcb8e5160a1c9764dd35446f4de1c7c649b3e7
--- app/src/main/java/com/yuricfurusho/temp01firebase/MainActivityFragment.kt
+++ app/src/main/java/com/yuricfurusho/temp01firebase/MainActivityFragment.kt
@@ -1,18 +1,29 @@
 package com.yuricfurusho.temp01firebase
 
-import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.analytics.FirebaseAnalytics
+import kotlinx.android.synthetic.main.fragment_main.*
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
 class MainActivityFragment : Fragment() {
 
+    private lateinit var mFirebaseAnalytics: FirebaseAnalytics
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this.context!!)
+
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "evento")
+        btnEvento.setOnClickListener { mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle) }
+
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 }
